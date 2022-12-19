cd9d8106ab5116d5c3a808b3f3b6916cd30182c4
“andreapennisi04” <andreapennisi04@gmail.com>
2022-12-19 23:19:51 +0000

    entrega 20/diciembre/2022

--- js/script.js
+++ js/script.js
@@ -14,22 +14,22 @@ const metrosCuadradosMaximo = 12;
 const metrosCuadradoPromedioPorAsiento =
   (metrosCuadradosMinimo + metrosCuadradosMaximo) / 2;
 
-function validarCupon(descuento) {
-  switch (descuento) {
-    case "FELIZNAVIDAD":
-      return 5;
-      break;
-    case "FELIZANIONUEVO":
-      return 15;
-      break;
-    case "BLACKFRIDAY":
-      return 10;
-      break;
-    default:
-      return 0;
-      break;
-  }
+function darBienvenida(saludo) {
+  alert("Bienvenido " + saludo + ". Usted esta por hacer un presupuesto");
 }
+
+let nombreIngresado = "";
+let apellidoIngresado = "";
+
+do {
+  nombreIngresado = prompt("Ingresar nombre");
+  apellidoIngresado = prompt("Ingresar apellido");
+  if (nombreIngresado != "" && apellidoIngresado != "") {
+    darBienvenida(nombreIngresado + " " + apellidoIngresado);
+  } else {
+    alert("Error: Debe ingresar nombre y apellido");
+  }
+} while (nombreIngresado === "" || apellidoIngresado === "");
 
 function factorDePrecioPorColor(color) {
   switch (color) {
@@ -57,48 +57,104 @@ function factorDePrecioPorColor(color) {
   }
 }
 
-function darBienvenida(saludo) {
-  alert("Bienvenido " + saludo + ". Usted esta por hacer un presupuesto");
+function factorDePrecioPorEstiloCostura(estiloDeCostura) {
+  const estilos = ["nada", "simpleRecta", "dobleDobleRecta", "zigzag", "punto"];
+  const factoresEstilos = [0, 1, 1.3, 1.2, 1.15];
+  const posicionDelPrecio = estilos.indexOf(estiloDeCostura);
+  return factoresEstilos[posicionDelPrecio];
 }
 
-let nombreIngresado = "";
-let apellidoIngresado = "";
-
-do {
-  nombreIngresado = prompt("Ingresar nombre");
-  apellidoIngresado = prompt("Ingresar apellido");
-  if (nombreIngresado != "" && apellidoIngresado != "") {
-    darBienvenida(nombreIngresado + " " + apellidoIngresado);
-  } else {
-    alert("Error: Debe ingresar nombre y apellido");
+const factorDePrecioPorColorCostura = (colorCostura) => {
+  switch (colorCostura) {
+    case "negrosCostura":
+      return 1.1;
+      break;
+    case "cremaCostura":
+    case "blancoCostura":
+      return 1.15;
+      break;
+    case "grafitoCostura":
+      return 1.2;
+      break;
+    case "grisCostura":
+      return 1.18;
+      break;
+    case "combinadoCostura":
+      return 1.25;
+      break;
+    default:
+      return 0;
+      break;
   }
-} while (nombreIngresado === "" || apellidoIngresado === "");
+};
+
+function validarCupon(descuento) {
+  switch (descuento) {
+    case "FELIZNAVIDAD":
+      return 5;
+      break;
+    case "FELIZANIONUEVO":
+      return 15;
+      break;
+    case "BLACKFRIDAY":
+      return 10;
+      break;
+    default:
+      return 0;
+      break;
+  }
+}
 
 function hacerPresupuesto() {
+  const estiloDeCostura =
+    window.document.getElementById("estiloDeCostura").value;
+  if (estiloDeCostura === "nada") {
+    alert("Seleccione estilo de costura");
+    return;
+  }
+
+  const colorCostura = window.document.getElementById("colorCostura").value;
+  if (colorCostura === "nada") {
+    alert("Seleccione color de costura");
+    return;
+  }
+
   const cuponDeDescuento = window.document.getElementById("cupon").value;
   const descuento = validarCupon(cuponDeDescuento);
   window.document.getElementById("descuento").innerHTML = descuento;
+
   const asientos = window.document.getElementById("asientos").value;
   if (asientos === "nada") {
     alert("Seleccione cantidad de asientos");
     return;
   }
+
   const textura = window.document.getElementById("textura").value;
   if (textura === "nada") {
     alert("Seleccione textura");
     return;
   }
+
   const colorTapizado = window.document.getElementById("colorTapizado").value;
   if (colorTapizado === "nada") {
     alert("Seleccione color de tapizado");
     return;
   }
+
   let precioSubTotal =
     precioMetroCuadradoTextura[textura] *
     asientos *
     factorDePrecioPorColor(colorTapizado) *
-    metrosCuadradoPromedioPorAsiento;
-  window.document.getElementById("sub-total").innerHTML = precioSubTotal;
+    metrosCuadradoPromedioPorAsiento *
+    factorDePrecioPorEstiloCostura(estiloDeCostura) *
+    factorDePrecioPorColorCostura(colorCostura);
+
+  precioSubTotal = Math.round(precioSubTotal);
+
   let precioTotal = ((100 - descuento) * precioSubTotal) / 100;
+
+  precioTotal = Math.round(precioTotal);
+
+  window.document.getElementById("sub-total").innerHTML = precioSubTotal;
   window.document.getElementById("total").innerHTML = precioTotal;
 }
